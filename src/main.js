69b20a23fb6fb142b3e3bc34b5e2a4a17bae2ef0
Pranay Bramhne <pranaybramhne@gmail.com>
2023-06-13 15:44:11 +0530

    async and await

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-//blocking or nonblocking?
 async function sum(n1,n2) {
     return n1+n2;
 }
 
-let result=sum(10,20);
-console.log(result);
+async function main(){
+    let result= await sum(10,20);
+    console.log(result);
+}
+
+main();
